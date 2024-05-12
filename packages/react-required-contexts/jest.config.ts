bf0f2accf837093213defa7ffbbb80aaf57b36a2
Mikael Labrut <labrut@gmail.com>
2024-05-12 17:59:55 +0200

    fix: nx config, nx build, test, lint, release and ci

--- packages/react-required-contexts/jest.config.ts
+++ packages/react-required-contexts/jest.config.ts
@@ -2,10 +2,5 @@
 export default {
   displayName: 'react-required-contexts',
   preset: '../../jest.preset.js',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
-  },
-  moduleFileExtensions: ['ts', 'js', 'html'],
   coverageDirectory: '../../coverage/packages/react-required-contexts',
 };
